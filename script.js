cf31c93008ddda9bc21677d6aef545bff7c358f1
Eshuyaleo <115549948+Eshuyaleo@users.noreply.github.com>
2023-02-02 14:26:45 +0100

    updated the script.js file

--- script.js
+++ script.js
@@ -4,12 +4,84 @@ function getComputerChoice(){
     return options[Math.floor(Math.random() * options.length)];
 }
 
-let playerSelection = prompt("rock paper scissors?").toLowerCase();
 let computerChoice = getComputerChoice();
+
+function playerChoice(){
+   let playerOptions = prompt("rock, paper,scissors?").toLowerCase();
+   return playerOptions
+}
+let playerSelection = playerChoice();
+
+let playerScore = 0;
+let computerScore = 0;
+let round = playerScore + computerScore + 1;
+
+function playRound(playerSelection, computerChoice, playerScore, computerScore, round){
+
+   if(playerSelection === computerChoice){
+      return ("it`s tie");
+   } 
+   else if((playerSelection === "rock" && computerChoice === "scissors") || 
+   (playerSelection === "paper" && computerChoice === "rock") ||
+   (playerSelection === "scissors" && computerChoice === "paper")
+   ){
+      ++playerScore;
+      return (`you won round ${round} the score is ${playerScore} to ${computerScore}`)
+   }
+   else if ((playerSelection === "rock" && computerChoice === "paper")||
+   (playerSelection === "paper" && computerChoice === "scissors")||
+   (playerSelection === " scissors" && computerChoice === "rock")){
+      ++computerScore;
+      return(`you lost round ${round} the score is ${playerScore} to ${computerScore}`);
+   }
+   else{
+      console.log("invalid input");
+   }
+      
+}
+oneRound = playRound(playerSelection, computerChoice, playerScore, computerScore, round)
+ function game(oneRound){
+   for(let i = 0; i <= 5; i++){
+
+   }
+   playerSelection;
+   return oneRound;
+ }
+ console.log(game(oneRound));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  let playerSelection = prompt("rock paper scissors?").toLowerCase();
 
 
 function playRound(playerSelection, computerChoice){
-
+    
     if(playerSelection === "rock" && computerChoice === "scissors"){
        return(` the computer chose ${computerChoice} and you chose ${playerSelection} you win `)
     }
@@ -20,7 +92,7 @@ function playRound(playerSelection, computerChoice){
        return(` the computer chose ${computerChoice} and you chose ${playerSelection} you win `)
     }
     else if (playerSelection === "rock" && computerChoice === "paper"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you lose `)
+       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you lose ` )
     }
     else if (playerSelection === "paper" && computerChoice === "scissors"){
        return(` the computer chose ${computerChoice} and you chose ${playerSelection} you lose `)
@@ -41,7 +113,14 @@ function playRound(playerSelection, computerChoice){
        return(" invalid input ")
     }
 }
+let computerScore = 0;
+let playerScore = 0;
+play = playRound(playerSelection, computerChoice)
 
-let round = playRound(playerSelection, computerChoice);
+function game(play){
+    for(let i = 0; i <= 5; i++){
+    }
+    return playRound(playerSelection, computerChoiceS)
 
-console.log(round);
+}
+console.log(game(play)); */
