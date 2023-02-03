44854e26c2926a77e46aa740f882f08b7f97e92c
Eshuyaleo <115549948+Eshuyaleo@users.noreply.github.com>
2023-02-03 17:28:03 +0100

    updated all files

--- script.js
+++ script.js
@@ -1,126 +1,36 @@
-let options = ["rock", "paper", "scissors"]
+let playerScore = document.querySelector(".player")
+let computerScore = document.querySelector(".com")
+let result = document.querySelector(".result")
+let choices = document.querySelectorAll(".choices")
+let options = ["ROCK", "PAPER","SCISSORS"];
+let player;
+let com;
+
+choices.forEach(button => button.addEventListener("click", () => {
+   player = button.textContent;
+   getComputerChoice();
+   playerScore.textContent = `Player score: ${player}`;
+   computerScore.textContent = `Com score: ${computerChoice}`;
+   result.textContent = playRound();
+}));
 
 function getComputerChoice(){
-    return options[Math.floor(Math.random() * options.length)];
+   let option = options[Math.floor(Math.random()*options.length)];
+   return option
 }
+computerChoice = getComputerChoice();
 
-let computerChoice = getComputerChoice();
-
-function playerChoice(){
-   let playerOptions = prompt("rock, paper,scissors?").toLowerCase();
-   return playerOptions
-}
-let playerSelection = playerChoice();
-
-let playerScore = 0;
-let computerScore = 0;
-let round = playerScore + computerScore + 1;
-
-function playRound(playerSelection, computerChoice, playerScore, computerScore, round){
-
-   if(playerSelection === computerChoice){
-      return ("it`s tie");
-   } 
-   else if((playerSelection === "rock" && computerChoice === "scissors") || 
-   (playerSelection === "paper" && computerChoice === "rock") ||
-   (playerSelection === "scissors" && computerChoice === "paper")
-   ){
-      ++playerScore;
-      return (`you won round ${round} the score is ${playerScore} to ${computerScore}`)
+function playRound(){
+   if(player === computerChoice){
+      return "it's a tie"
    }
-   else if ((playerSelection === "rock" && computerChoice === "paper")||
-   (playerSelection === "paper" && computerChoice === "scissors")||
-   (playerSelection === " scissors" && computerChoice === "rock")){
-      ++computerScore;
-      return(`you lost round ${round} the score is ${playerScore} to ${computerScore}`);
-   }
-   else{
-      console.log("invalid input");
-   }
-      
+   else if((player === "ROCK" && computerChoice === "SCISSORS")||(
+      player === "PAPER" && computerChoice === "ROCK")||
+      (player === "SCISSORS" && computerChoice === "PAPER"))
+      return `you win`
+   else if((player === "ROCK" && computerChoice === "PAPER")||
+      (player === "PAPER" && computerChoice === "SCISSORS")||
+      (player === "SCISSORS" && computerChoice === "ROCK"))
+      return "you lose"
 }
-oneRound = playRound(playerSelection, computerChoice, playerScore, computerScore, round)
- function game(oneRound){
-   for(let i = 0; i <= 5; i++){
 
-   }
-   playerSelection;
-   return oneRound;
- }
- console.log(game(oneRound));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  let playerSelection = prompt("rock paper scissors?").toLowerCase();
-
-
-function playRound(playerSelection, computerChoice){
-    
-    if(playerSelection === "rock" && computerChoice === "scissors"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you win `)
-    }
-    else if (playerSelection === "paper" && computerChoice === "rock"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you win `)
-    }
-    else if (playerSelection === "scissors" && computerChoice === "paper"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you win `)
-    }
-    else if (playerSelection === "rock" && computerChoice === "paper"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you lose ` )
-    }
-    else if (playerSelection === "paper" && computerChoice === "scissors"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you lose `)
-    }
-    else if (playerSelection === "scissors" && computerChoice === "rock"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} you lose `)
-    }
-    else if (playerSelection === "rock" && computerChoice === "rock"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} it's a draw `)
-    }
-    else if (playerSelection === "paper" && computerChoice === "paper"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} it's a draw `)
-    }
-    else if (playerSelection === "scissors" && computerChoice === "scissors"){
-       return(` the computer chose ${computerChoice} and you chose ${playerSelection} it's a draw `)
-    }
-    else{
-       return(" invalid input ")
-    }
-}
-let computerScore = 0;
-let playerScore = 0;
-play = playRound(playerSelection, computerChoice)
-
-function game(play){
-    for(let i = 0; i <= 5; i++){
-    }
-    return playRound(playerSelection, computerChoiceS)
-
-}
-console.log(game(play)); */
